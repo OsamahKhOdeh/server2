ab701dd7d3a7778b4b453f4f9723fff37aa7e46c
OsamahKhOdeh <eng.osama.odeh@gmail.com>
2023-06-07 20:17:51 +0400

    cors u

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -6,6 +6,9 @@ const allowedOrigins = [
   "https://143.42.61.215/warranty",
   "https://143.42.61.215/check",
   "https://143.42.61.215/login",
+  "http://products.agsdubai.com",
+  "http://143.42.61.215:5000/warranty",
+  "http://products.agsdubai.com/login",
 ];
 
 export default allowedOrigins;

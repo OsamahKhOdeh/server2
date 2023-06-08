21f7c3429f7b25b281ccce552948fff6b08d76bd
OsamahKhOdeh <eng.osama.odeh@gmail.com>
2023-06-08 15:47:07 +0400

    add origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -9,6 +9,12 @@ const allowedOrigins = [
   "http://products.agsdubai.com",
   "http://143.42.61.215:5000/warranty",
   "http://products.agsdubai.com/login",
+  "ags-sales.com",
+  "ags-sales.com/check",
+  "ags-sales.com/products",
+  "hmcfz.co",
+  "hmcfz.co/check",
+  "hmcfz.co/products",
 ];
 
 export default allowedOrigins;
